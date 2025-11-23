46ccdc4e44a447c56906840e6c5138bebb544d4f
shakiburrashid <shakiburrashid7@gmail.com>
2025-11-23 11:06:22 +0600

    Problem 7 solved

--- Problem6.ts
+++ Problem6.ts
@@ -9,11 +9,3 @@ function printBookDetails(value:Book){
     return console.log(`Title: ${value.title}, Author: ${value.author}, Published: ${value.publishedYear}, Available: ${(value.isAvailable?`Yes`:`No`)}`)
 } 
 
-const myBook: Book = {
-  title: 'The Great Gatsby',
-  author: 'F. Scott Fitzgerald',
-  publishedYear: 1985,
-  isAvailable: true,
-};
-
-printBookDetails(myBook);
---
+++ Problem7.ts
@@ -0,0 +1,8 @@
+
+
+
+function getUniqueValues(value1:number[],value2:number[]):number[]{
+    return Array.from(new Set([...value1, ...value2]));
+
+}
+
