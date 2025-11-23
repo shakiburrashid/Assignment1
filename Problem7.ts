e17f053ebc705dec0a9cb99a3fe6e08ff7f5688e
shakiburrashid <shakiburrashid7@gmail.com>
2025-11-23 11:20:37 +0600

    Probem 1 to 7 solved

--- Problem7.ts
+++
@@ -1,8 +0,0 @@
-
-
-
-function getUniqueValues(value1:number[],value2:number[]):number[]{
-    return Array.from(new Set([...value1, ...value2]));
-
-}
-
---
+++ solution.ts
@@ -0,0 +1,98 @@
+//* Problem 1 -----------
+
+const formatValue = (value: string | number | boolean) => {
+    if (typeof value === 'string') {
+        return value.toUpperCase()
+    }
+    if (typeof value === 'number') {
+        return value * 10
+    }
+    if (typeof value === 'boolean') {
+        return !value
+    }
+}
+
+
+//* Problem 2 -----------
+
+const getLength = (value: string | object) => {
+
+    if (typeof value === 'string') {
+        return value.length
+    }
+    if (Array.isArray(value)) {
+        return value.length;
+    }
+}
+
+
+
+//* Problem 3 -----------
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name,
+            this.age = age
+    }
+
+    getDetails() {
+        return (`Name: ${this.name}, Age: ${this.age}`)
+    }
+}
+
+ 
+//* Problem 4 -----------
+
+type value = {
+    title: string;
+    rating: number;
+};
+
+function filterByRating(Value: value[]): value[] {
+    return Value.filter(data => data.rating >= 4);
+}
+
+//* Problem 5 -----------
+
+type UserData = {
+    id: number,
+    name: string,
+    email: string,
+    isActive: boolean
+}
+
+
+function filterActiveUsers(value: UserData[]): UserData[] {
+    return value.filter(data => data.isActive)
+}
+
+
+//* Problem 6 -----------
+
+interface Book {
+    title: string,
+    author: string,
+    publishedYear: number,
+    isAvailable: boolean
+}
+
+function printBookDetails(value:Book){
+    return console.log(`Title: ${value.title}, Author: ${value.author}, Published: ${value.publishedYear}, Available: ${(value.isAvailable?`Yes`:`No`)}`)
+} 
+
+//* Problem 7 -----------
+
+function getUniqueValues(value1:number[],value2:number[]):number[]{
+    return Array.from(new Set([...value1, ...value2]));
+
+}
+
+
+
+//* Problem 8 -----------
+
+
+
+
